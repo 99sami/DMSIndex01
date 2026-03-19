30a026b28ec2d5850449d7d4c0c39fc1dca56370
99sami <s3851083@student.rmit.edu.au>
2026-03-19 15:55:41 +1100

    experiment / added foler
    did an experiment for dots, added it into my main indexhtml for A1

---
+++ mappingDesignExperiment/mappingMain.js
@@ -0,0 +1,39 @@
+// find our elements
+const stageContainer = document.getElementById("stage-container");
+const circleButton = document.getElementById("circle-button");
+
+let stageContainerWidth = stageContainer.offsetWidth;
+// console.log(stageContainerWidth);
+// find our height
+let stageContainerHeight = stageContainer.offsetHeight;
+// console.log(stageContainerHeight)
+
+// set default circle colour
+let circleColour = "red";
+
+// create the konca stage
+const stage = new Konva.Stage({
+  container: "konva-stage",
+  width: stageContainerWidth,
+  height: stageContainerHeight,
+});
+// create our layer
+const firstLayer = new Konva.Layer();
+
+// add the layer to our stage
+stage.add(firstLayer);
+
+// add interaction to button
+function drawNewCircle() {
+  const circle = new Konva.Circle({
+    x: stage.width() * Math.random(),
+    y: stage.height() * Math.random(),
+    radius: 50 * Math.random(),
+    fill: circleColour,
+  });
+
+  // add the circle to our first layer
+  firstLayer.add(circle);
+}
+
+circleButton.addEventListener("click", drawNewCircle);
